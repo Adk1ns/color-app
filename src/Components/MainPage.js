c708230bf5c2f0eefb54e2b6b34fcf898021a2d3
Jered Adkins <jeredkins@gmail.com>
2022-06-08 18:03:26 -0700

    refactored filter buttons

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -7,6 +7,7 @@ import ColorScale from './ColorScale/ColorScale'
 
 const MainPage = ({ mySwatches, setMySwatches }) => {
 	const [popSwatches, setPopSwatches] = useState(SwatchesData.SwatchesData)
+	const filters = ['green', 'yellow', 'red', 'blue', 'purple', 'orange', 'brown', 'pink', 'gray', 'teal', 'autumn', 'summer', 'spring', 'winter', 'bold', 'light', 'dark']
 
 	const sortColor = (filter) => {
 		if (popSwatches) {
@@ -21,75 +22,14 @@ const MainPage = ({ mySwatches, setMySwatches }) => {
 		}
 	}
 
-	//TODO: re-factor buttons
-
 	return (
 		<div className='mx-3'>
 			<div className='filter-button row d-flex justify-content-center'>
-				<div></div>
-				<button onClick={() => sortColor('green')} className='box-shadow'>
-					Green
-				</button>
-				<button onClick={() => sortColor('yellow')} className='box-shadow'>
-					Yellow
-				</button>
-				<button onClick={() => sortColor('red')} className='box-shadow'>
-					Red
-				</button>
-				<button onClick={() => sortColor('blue')} className='box-shadow'>
-					Blue
-				</button>
-				<button onClick={() => sortColor('purple')} className='box-shadow'>
-					Purple
-				</button>
-				<button onClick={() => sortColor('orange')} className='box-shadow'>
-					Orange
-				</button>
-				<button onClick={() => sortColor('brown')} className='box-shadow'>
-					Brown
-				</button>
-				<button onClick={() => sortColor('pink')} className='box-shadow'>
-					Pink
-				</button>
-				<button onClick={() => sortColor('gray')} className='box-shadow'>
-					Grey
-				</button>
-				<button onClick={() => sortColor('black')} className='box-shadow'>
-					Black
-				</button>
-				<button onClick={() => sortColor('popular')} className='box-shadow'>
-					Popular
-				</button>
-				<button onClick={() => sortColor('autumn')} className='box-shadow'>
-					Autumn
-				</button>
-				<button onClick={() => sortColor('summer')} className='box-shadow'>
-					Summer
-				</button>
-				<button onClick={() => sortColor('spring')} className='box-shadow'>
-					Spring
-				</button>
-				<button onClick={() => sortColor('winter')} className='box-shadow'>
-					Winter
-				</button>
-				<button onClick={() => sortColor('bold')} className='box-shadow'>
-					Bold
-				</button>
-				<button onClick={() => sortColor('trending')} className='box-shadow'>
-					Trending
-				</button>
-				<button onClick={() => sortColor('new')} className='box-shadow'>
-					New
-				</button>
-				<button onClick={() => sortColor('teal')} className='box-shadow'>
-					Teal
-				</button>
-				<button onClick={() => sortColor('light')} className='box-shadow'>
-					Light
-				</button>
-				<button onClick={() => sortColor('dark')} className='box-shadow'>
-					Dark
-				</button>
+				{filters.map((colors, index) => 
+					<button onClick={() => sortColor(colors)} className='box-shadow' key={index}>
+						{colors}
+					</button>
+				)}
 			</div>
 			<div className='d-flex flex-row'>
 				<div className='col-10 mt-4'>
